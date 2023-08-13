298ae41c08a563b2ccf47d74a98ac8e05d3fb4e9
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-13 11:07:48 -0300

    feat: criação do menu side e top a depender do tamanho da tela

--- src/Pages/ForjaDeLendas/Forja/ForjaCriacao/componentes/BannerItem/index.tsx
+++ src/Pages/ForjaDeLendas/Forja/ForjaCriacao/componentes/BannerItem/index.tsx
@@ -7,7 +7,7 @@ import Habilidade from './Habilidade';
 
 function BannerItem({ nome, descricao, habilidades }: IBannerItem) {
     return (
-        <section className={styles.bannerItem}>
+        <section key={descricao} className={styles.bannerItem}>
             <h3 className={styles.bannerItem__titulo}>{nome}</h3>
             <p className={styles.bannerItem__descricao}>{descricao}</p>
             {habilidades ? habilidades.map(e => <Habilidade {...e} />) : ''}
--- src/Pages/ForjaDeLendas/Forja/ForjaCriacao/componentes/MenuTop/index.tsx
+++ src/Pages/ForjaDeLendas/Forja/ForjaCriacao/componentes/MenuTop/index.tsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import IBannerItem from '../../interface/IBannerItem';
+import useSetBannerMostrado from 'Pages/ForjaDeLendas/Forja/state/hooks/useSetBannerMostrado';
+import styles from './MenuTop.module.scss';
+import { ImArrowLeft2, ImArrowRight2 } from 'react-icons/im';
 
 interface MenuDeSelecaoProps {
   Opcoes: IBannerItem[];
 }
 
-function MenuTop({Opcoes}: MenuDeSelecaoProps) {
+function MenuTop({ Opcoes }: MenuDeSelecaoProps) {
+  const setBanner = useSetBannerMostrado();
+
+  const [lugarNaFila, setLugarNaFila] = useState<number>();
+
+
+  useEffect(() => {
+    setLugarNaFila(0)
+  }, []);
+
+  useEffect(() => {
+    setBanner(Opcoes[lugarNaFila!])
+  }, [lugarNaFila]);
+
+
+
+  const traz = () => {
+    if (lugarNaFila === 0) {
+      setLugarNaFila(Opcoes.length - 1)
+    } else {
+      setLugarNaFila(lugarNaFila! - 1)
+    }
+  }
+
+  const frente = () => {
+    if (lugarNaFila === Opcoes.length - 1) {
+      setLugarNaFila(0)
+    } else {
+      setLugarNaFila(lugarNaFila! + 1)
+    }
+  }
+
   return (
-    <div>MenuTop</div>
+    <div className={styles.menuTop}>
+      <button onClick={traz}><ImArrowLeft2 /></button>
+      {Opcoes[lugarNaFila!] ? <p>{Opcoes[lugarNaFila!].nome}</p> : ''}
+      <button onClick={frente}><ImArrowRight2 /></button>
+    </div>
   )
 }
 
