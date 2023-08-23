6100c9f047d1fe345a51b61ec9b3b11e5d3b6a4a
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-22 23:13:59 -0300

    fix: final dia 22/08

---
+++ src/Pages/ForjaDeLendas/components/BtnForja/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./BtnForja.module.scss";
+
+function BtnForja() {
+  return (
+    <button className={styles.btn} >Forja</button>
+  )
+}
+
+export default BtnForja
