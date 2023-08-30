46b6385d7694a05ab2d4f5914f103269e176f532
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-29 21:07:05 -0300

    feat: finalização da tela de carregamento e correção de bugs vizuais

--- src/RouteAPP.tsx
+++ src/RouteAPP.tsx
@@ -1,39 +1,54 @@
-import Antecedente from "Pages/ForjaDeLendas/Forja/Antecedente";
-import Classe from "Pages/ForjaDeLendas/Forja/Classe";
-import ForjaCriacao from "Pages/ForjaDeLendas/Forja/ForjaCriacao";
-import Forja from "Pages/ForjaDeLendas/Forja/Nomes";
-import Racas from "Pages/ForjaDeLendas/Forja/Racas";
-import ForjaComum from "Pages/ForjaDeLendas/ForjaComum";
-import ForjaHome from "Pages/ForjaDeLendas/ForjaHome";
-import ForjaPersonagens from "Pages/ForjaDeLendas/ForjaPersonagens";
-import Ficha from "Pages/ForjaDeLendas/ForjaPersonagens/Ficha";
-import GoblinCaolho from "Pages/GoblinCaolho";
-import Home from "Pages/Home";
+import Carregamento from "Components/Carregamento";
 import PaginaBase from "Pages/PaginaBase";
+import Home from "Pages/Home";
+
+
+// import Antecedente from "Pages/ForjaDeLendas/Forja/Antecedente";
+// import Classe from "Pages/ForjaDeLendas/Forja/Classe";
+// import ForjaCriacao from "Pages/ForjaDeLendas/Forja/ForjaCriacao";
+// import Forja from "Pages/ForjaDeLendas/Forja/Nomes";
+// import Racas from "Pages/ForjaDeLendas/Forja/Racas";
+// import ForjaComum from "Pages/ForjaDeLendas/ForjaComum";
+// import ForjaHome from "Pages/ForjaDeLendas/ForjaHome";
+// import ForjaPersonagens from "Pages/ForjaDeLendas/ForjaPersonagens";
+// import Ficha from "Pages/ForjaDeLendas/ForjaPersonagens/Ficha";
+import GoblinCaolho from "Pages/GoblinCaolho";
 import PaginaErro from "Pages/PaginaErro";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+
+const Antecedente = lazy(() => import('Pages/ForjaDeLendas/Forja/Antecedente'));
+const Classe = lazy(() => import('Pages/ForjaDeLendas/Forja/Classe'));
+const ForjaCriacao = lazy(() => import('Pages/ForjaDeLendas/Forja/ForjaCriacao'));
+const Forja = lazy(() => import('Pages/ForjaDeLendas/Forja/Nomes'));
+const Racas = lazy(() => import('Pages/ForjaDeLendas/Forja/Racas'));
+const ForjaComum = lazy(() => import('Pages/ForjaDeLendas/ForjaComum'));
+const ForjaHome = lazy(() => import('Pages/ForjaDeLendas/ForjaHome'));
+const ForjaPersonagens = lazy(() => import('Pages/ForjaDeLendas/ForjaPersonagens'));
+const Ficha = lazy(() => import('Pages/ForjaDeLendas/ForjaPersonagens/Ficha'));
 
 function RouteAPP() {
 	return (
-		<Routes>
-			<Route path="/" element={<PaginaBase />}>
-				<Route index element={<Home />} />
-				<Route path="goblin-caolho" element={<GoblinCaolho />} />
-				<Route path="forja-de-lendas" element={<ForjaComum />}>
-					<Route index element={<ForjaHome />} />
-					<Route path="forja" element={<Forja />} />
-					<Route path="personagens" element={<ForjaPersonagens />} />
-					<Route path="ficha" element={<Ficha />} />
+		<Suspense fallback={<Carregamento />}>
+			<Routes>
+				<Route path="/" element={<PaginaBase />}>
+					<Route index element={<Home />} />
+					<Route path="goblin-caolho" element={<GoblinCaolho />} />
+					<Route path="forja-de-lendas" element={<ForjaComum />}>
+						<Route index element={<ForjaHome />} />
+						<Route path="forja" element={<Forja />} />
+						<Route path="personagens" element={<ForjaPersonagens />} />
+						<Route path="ficha" element={<Ficha />} />
+					</Route>
+					<Route path="forja/cricao" element={<ForjaCriacao />}>
+						<Route path="raca" element={<Racas />} />
+						<Route path="antecedente" element={<Antecedente />} />
+						<Route path="classe" element={<Classe />} />
+					</Route>
 				</Route>
-				<Route path="forja/cricao" element={<ForjaCriacao />}>
-					<Route path="raca" element={<Racas />}/>
-					<Route path="antecedente" element={<Antecedente />}/>
-					<Route path="classe" element={<Classe />}/>
-				</Route>
-			</Route>
-			<Route path="*" element={<PaginaErro />} />
-		</Routes>
+				<Route path="*" element={<PaginaErro />} />
+			</Routes>
+		</Suspense>
 	);
 }
 
